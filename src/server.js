18918882432f70b890d8deec4cea8f1024c59721
Kyano Debeuckelaere <kyano.debeuckelaere12@gmail.com>
2025-12-22 11:00:55 +0100

    try

--- src/server.js
+++ src/server.js
@@ -25,6 +25,6 @@ setupSocket(io);
 // Start HTTP server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server listening on 0.0.0.0:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
